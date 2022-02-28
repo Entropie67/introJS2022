e5ca0b2cdc8176edfb3c58ad1c27f0510f752a8a
Entropie67 <olivier.elophe@gmail.com>
2022-02-28 14:03:50 +0100

    Bases de JS

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+console.log("Bonjour, je suis un message");
+document.write("ICI");
+
+// Syntaxe pour une simple boucle
+for (let i=0; i<10; i++){
+    console.log(i);
+}
+
+// Syntaxe pour une condition
+let age = 12;
+
+if (age <= 18){
+    console.log("OK");
+}else {
+    console.log("Trop vieux !");
+}
+
+// Ajout d'un paragrpahe
+let bloc = document.querySelector("#main");
+bloc.innerHTML = "<p>paragraphe</p>";
+
+// ajout de plusieurs paragraphes avec une boucle
+let blocBoucle = document.querySelector("#main");
+for (let i=0; i<10; i++){
+    bloc.innerHTML = "<p>paragraphe</p>";
+}
